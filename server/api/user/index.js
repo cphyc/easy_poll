a36c1d44b44938980da1e8cadc7693b8e386258c
Corentin Cadiou <contact@cphyc.me>
2015-11-17 16:52:36 +0100

    Prepare field for answers

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -9,9 +9,9 @@ var router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
-router.get('/me', auth.isAuthenticated(), controller.me);
-router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
-router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', controller.create);
+router.get('/me', auth.hasRole('user'), controller.me);
+router.put('/:id/password', auth.hasRole('user'), controller.changePassword);
+router.get('/:id', auth.hasRole('user'), controller.show);
+router.post('/', auth.hasRole('admin'), controller.create);
 
 module.exports = router;
--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/answer/answer.socket').register(socket);
   require('../api/poll/poll.socket').register(socket);
 }
 
