ccc31926899fc1cb7b6004a7d0baef900ab4122a
Corentin Cadiou <contact@cphyc.me>
2015-11-17 20:22:18 +0100

    Can now answer to a poll

--- client/app/answer_poll/answer_poll.controller.js
+++ client/app/answer_poll/answer_poll.controller.js
@@ -10,18 +10,35 @@ angular.module('eduquizzApp')
     }
   })
   .controller('AnswerPollCtrl', function($scope, $stateParams, Poll, Answer, Auth) {
+    $scope.questionNumber = 0;
     $scope.poll = Poll.get({id: $stateParams.id}, function() {
       // Create an answer object
-      $scope.answers = new Array($scope.poll.questions.length - 1);
+      $scope.answers         = new Array($scope.poll.questions.length - 1);
+      $scope.currentQuestion = $scope.poll.questions[$scope.questionNumber];
+      $scope.lastQuestion    = $scope.poll.questions.length - 1;
     });
 
+    $scope.nextQuestion = function() {
+      $scope.questionNumber += 1;
+      $scope.currentQuestion = $scope.poll.questions[$scope.questionNumber];
+    }
+
+    $scope.saveButton = 'Save answers';
     $scope.saveAnswers = function() {
       if ($scope.answers) {
-        new Answer({
+        $scope.saveButton = 'Saving answers…';
+        var foo = new Answer({
           user: Auth.getCurrentUser()._id,
           poll: $scope.poll._id,
           answers: $scope.answers
-        }).$save();
+        })
+        .$save(function() {
+          $scope.saveButton = 'Answers saved!';
+          $scope.submitted = true;
+        })
+        .catch(function(a) {
+          $scope.saveButton = 'Error while saving. Try again.';
+        });
       }
     };
   });
