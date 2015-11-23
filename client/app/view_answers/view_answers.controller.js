17949376685f0b5ea18bc4c171b0f2482862a5f7
Corentin Cadiou <contact@cphyc.me>
2015-11-23 19:10:07 +0100

    Prev work'

--- client/app/view_answers/view_answers.controller.js
+++ client/app/view_answers/view_answers.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('eduquizzApp')
-  .controller('ViewAnswersCtrl', function ($scope, $stateParams, Poll) {
+  .controller('ViewAnswersCtrl', function ($scope, $stateParams, Poll, Answer) {
     $scope.poll = Poll.get({id: $stateParams.pollId});
+    $scope.answers = Answer.get({id: $stateParams.pollId});
   });
