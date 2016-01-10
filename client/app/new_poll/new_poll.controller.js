87e3a3708f6d0fe46b2ce7dba44401a7a5b2947f
Corentin Cadiou <contact@cphyc.me>
2016-01-10 23:57:27 +0100

    change to md and simplify interface

--- client/app/new_poll/new_poll.controller.js
+++ client/app/new_poll/new_poll.controller.js
@@ -3,14 +3,15 @@
 angular.module('eduquizzApp')
   .controller('NewPollCtrl', function ($scope, pollInputService, Restangular, $location) {
     $scope.poll = {
-      name : '',
+      name : 'New poll',
+      before: 'Description of the poll',
       questions: [{
-        question: '',
-        answers: [],
+        question: 'Question',
+        answers: ['Some answer'],
         answer: 0
-      }]
+      }],
+      after: 'End of the poll'
     };
-
     var nullValue = { question : '', answer: null, answers: []};
     $scope.new_question = {value: angular.copy(nullValue)};
     $scope.validate_new_question = pollInputService.validatorGenerator('Expecting new question!', angular.copy(nullValue));
@@ -25,18 +26,49 @@ angular.module('eduquizzApp')
     };
 
   })
-  .controller('EditPollCtrl', function ($scope, pollInputService, Restangular, $stateParams, $location) {
+  .controller('EditPollCtrl', function ($scope, Restangular, $stateParams, $location) {
     $scope.poll = Restangular.one('polls', $stateParams.id).get().$object;
 
-    var nullValue = { question : '', answer: null, answers: []};
-    $scope.new_question = {value: angular.copy(nullValue)};
-    $scope.validate_new_question = pollInputService.validatorGenerator('Expecting new question!', angular.copy(nullValue));
-    $scope.push_question = pollInputService.pusherGenerator($scope.new_question,
-      angular.copy(nullValue));
+    $scope.moveQuestion = function(index, offset) {
+      if ((index > 0 && offset === -1) || (index < question.answers.length-1 && offset === 1)) {
+        var question = $scope.poll.questions[index];
+        var otherQuestion = $scope.poll.questions[index+offset];
+        $scope.poll.questions[index+offset] = question;
+        $scope.poll.questions[index] = otherQuestion;
+      }
+    }
+
+    $scope.addAnswer = function(question) {
+      question.answers.push('New answer');
+    }
+
+    $scope.moveAnswer = function(question, index, offset) {
+      if ((index > 0 && offset === -1) || (index < question.answers.length-1 && offset === 1)) {
+        var answer = question.answers[index];
+        var otherAnswer = question.answers[index+offset];
+        question.answers[index+offset] = answer;
+        question.answers[index] = otherAnswer;
+
+        if (question.answer === index+offset) {
+          question.answer = index;
+        } else if (question.answer === index) {
+          question.answer = index+offset;
+        }
+      }
+    }
+
+    $scope.deleteAnswer = function(question, index) {
+      question.answers.splice(index, 1);
+
+      // We change the answer iif question.answer is out of bound
+      if (question.answer > question.answers.length - 1 ) {
+        question.answer = question.answers.length - 1;
+      }
+    }
 
     $scope.save = function() {
       $scope.buttonText = 'Submitting…';
-      $scope.poll.put().then(function() {
+      Restangular.one('polls', $scope.poll._id).patch($scope.poll).then(function() {
         $location.path('/polls');
       });
     };
