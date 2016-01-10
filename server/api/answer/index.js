88650208bfeb08f8edc558d7ef3b3d6d62436466
Corentin Cadiou <contact@cphyc.me>
2016-01-10 18:22:15 +0100

    update routes

--- server/api/answer/index.js
+++ server/api/answer/index.js
@@ -7,12 +7,13 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
-router.get('/:pollId', auth.hasRole('admin'), controller.indexForOnePoll);
-router.get('/:pollId/:id', auth.hasRole('user'), controller.show);
-router.get('/:pollId/user/:userId', auth.hasRole('user'), controller.showByUser);
-router.post('/:pollId', auth.hasRole('user'), controller.create);
-router.put('/:pollId/:id', auth.hasRole('user'), controller.update);
-router.patch('/:pollId/:id', auth.hasRole('admin'), controller.update);
-router.delete('/:pollId/:id', auth.hasRole('admin'), controller.destroy);
+//router.get('/:pollId', auth.hasRole('admin'), controller.indexForOnePoll);
+router.get('/poll/:pollId', auth.hasRole('user'), controller.newAnswer);
+router.get('/poll/:pollId/answer/:id', auth.hasRole('user'), controller.show);
+router.get('/poll/:pollId/user/:userId', auth.hasRole('user'), controller.showByUser);
+router.post('/poll/:pollId/question/:question/:answer', auth.hasRole('user'), controller.saveAnswer);
+//router.put('/:pollId/:id', auth.hasRole('user'), controller.update);
+//router.patch('/:pollId/:id', auth.hasRole('admin'), controller.update);
+router.delete('/poll/:pollId/answer/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
