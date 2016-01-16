fb65eb7794c0575a57e35d8eb6588760c01d7585
Corentin Cadiou <contact@cphyc.me>
2016-01-16 18:31:11 +0100

    add possibility to remove answer

--- client/app/view_answers/view_answers.controller.js
+++ client/app/view_answers/view_answers.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('eduquizzApp')
-  .controller('ViewAnswersCtrl', function ($scope, $stateParams, $http, Restangular) {
+  .controller('ViewAnswersCtrl', function ($scope, $stateParams, $http, Restangular, gettextCatalog, $mdDialog) {
     $scope.poll = Restangular.one('polls', $stateParams.pollId).get().$object;
 
     Restangular.one('polls', $stateParams.pollId).getList('results').then(function(answers) {
@@ -22,6 +22,34 @@ angular.module('eduquizzApp')
       .then(function(csvString) {
         var blob = new Blob([csvString], {type: "text/plain;charset=utf-8"});
         saveAs(blob, "sondage.csv");
+      });
+    };
+
+    $scope.deleteAnswer = function(ans, ev) {
+      var cfg = {
+        title: gettextCatalog.getString('Would you like to delete this answer?'),
+        ariaLabel: gettextCatalog.getString('delete answer'),
+        confirm: gettextCatalog.getString('Confirm'),
+        cancel: gettextCatalog.getString('Cancel!'),
+      }
+      var confirm = $mdDialog.confirm()
+          .title(cfg.title)
+          .ariaLabel(cfg.ariaLabel)
+          .targetEvent(ev)
+          .ok(cfg.confirm)
+          .cancel(cfg.cancel);
+      $mdDialog.show(confirm).then(function() {
+        var target = Restangular.all('answers').one('poll', ans.poll._id).one('answer', ans.answer);
+        target.remove()
+        .then(function() {
+          $scope.updateAnswers();
+        });
+      });
+    };
+
+    $scope.updateAnswers = function() {
+      Restangular.one('polls', $stateParams.pollId).getList('results').then(function(answers) {
+        $scope.answers = answers;
       });
     };
 
